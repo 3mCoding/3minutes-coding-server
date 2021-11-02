dbd1b89a89d0d40a6ab7340f86a36e61a9e19071
LEE MINJI <m04j00@gmail.com>
2021-11-02 19:58:00 +0900

    table 설계

---
+++ db.sql
@@ -0,0 +1,15 @@
+create database tmc_db;
+
+use tmc_db
+
+create table user(
+    email VARCHAR(50) PRIMARY KEY,
+    student_num INT NOT NULL,
+    password VARCHAR(200) NOT NULL,
+	date DATETIME
+)
+
+create table question_step(
+    student_num INT PRIMARY KEY,
+    stemp INT NOT NULL
+)
