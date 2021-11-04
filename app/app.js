9a9ae578fe39fa6e5db9f8eb3c768c8f8314fa73
LEE MINJI <m04j00@gmail.com>
2021-11-04 23:28:55 +0900

    문제 예제 추가

--- app/app.js
+++ app/app.js
@@ -8,5 +8,5 @@ app.get('/', function(req, res){
     res.send('3분 코딩');
 })
 const path = require('path');
-app.use('/json', express.static(path.join(__dirname, './')));
+app.use('/json', express.static(path.join(__dirname, '/question')));
 app.listen(3000, () => console.log('3000번 포트 대기'));
