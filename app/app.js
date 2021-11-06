10ed5c44eb2775b5ac431254368a0b29342e7f8c
LEE MINJI <m04j00@gmail.com>
2021-11-07 02:52:40 +0900

    문제 - get 메소드 구현

--- app/app.js
+++ app/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 const userRouter = require('./router/user');
+const questionRouter = require('./router/question');
 app.use('/user', userRouter);
+app.use('/question', questionRouter);
 
 app.get('/', function(req, res){
     res.send('3분 코딩');
 })
-const path = require('path');
-app.use('/question', express.static(path.join(__dirname, '/question')));
 app.listen(3000, () => console.log('3000번 포트 대기'));
--- app/router/question.js
+++ app/router/question.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const router = express();
+const connection = require('../mysql');
 
+router.get('/', function(req, res){
+    res.send('question');
+})
+// /question/java?no=1
+router.get('/:type', function (req, res) {
+    let type = req.params.type;
+    let no = req.query.no;
+    let sql = 'select * from question where type = ? AND no = ?';
+    let params = [type, no];
+    console.log(type, no);
+    connection.query(sql, params, function (err, result) {
+        if (err) return res.sendStatus(400);
+  
+        res.json(result);
+        console.log("result : " + JSON.stringify(result));
+    });
+});
 
 module.exports = router;
