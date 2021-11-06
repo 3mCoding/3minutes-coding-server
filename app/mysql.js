170960e329911dad91603c009bf42b373036d3da
LEE MINJI <m04j00@gmail.com>
2021-11-06 23:18:25 +0900

    디비 수정

--- app/mysql.js
+++ app/mysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '0620',
+    password: 'min2021!',
     database: 'tmc_db'
 });
  
